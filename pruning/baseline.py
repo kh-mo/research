91c2a92e5adf71d9c8bb935b0f0f67cb9095bd4d
mosi <momkdh2@naver.com>
2019-08-22 07:04:30 +0900

    data

--- pruning/baseline.py
+++ pruning/baseline.py
@@ -1,9 +1,15 @@
-from torchvision import models
+'''
+pretrain model : https://pytorch.org/docs/stable/torchvision/models.html
+dataset : https://pytorch.org/docs/stable/torchvision/datasets.html
+
+'''
+
+from torchvision import models, datasets
 
 if __name__ == "__main__":
     pretrain_model = models.alexnet(pretrained=True)
 
-    test_data = ????
+    test_data = datasets.ImageNet(root="dataset/", split="val", download=True)
     test_label = ????
 
     pred = pretrain_model(test_data)
