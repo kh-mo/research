0baf5561370efac2972ed7fcf20e495249adf7d9
skmkh19 <skmkh19@gmail.com>
2019-10-15 19:01:54 +0900

    log variational prior

--- classification/bayesian.py
+++ classification/bayesian.py
@@ -1,3 +1,5 @@
+import math
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as f
@@ -8,11 +10,17 @@ class Gaussian(object):
         self.mu = mu
         self.rho = rho
         self.normal = torch.distributions.normal.Normal(loc=0, scale=1)
+        self.sigma = 0
 
     def sample(self):
         epsilon = self.normal.sample(self.rho.size()).to(device)
-        sigma = torch.log(1 + torch.exp(self.rho))
-        return self.mu + sigma * epsilon
+        self.sigma = torch.log(1 + torch.exp(self.rho))
+        return self.mu + self.sigma * epsilon
+
+    def log_prob(self, input):
+        return (-math.log(math.sqrt(2 * math.pi))
+                - torch.log(self.sigma)
+                - (((input - self.mu)/self.sigma)**2)/2).sum()
 
 class BayesianLinear(nn.Module):
     def __init__(self, in_dim, out_dim):
@@ -28,24 +36,22 @@ class BayesianLinear(nn.Module):
         self.bias = Gaussian(self.bias_mu, self.bias_rho)
 
     def forward(self, input):
-        print(input) #3
-        print(input.shape) #4
         weight = self.weight.sample()
         bias = self.bias.sample()
-        print(weight.shape)
-        print(bias.shape)
         return f.linear(input, weight, bias)
 
 class bayesian(nn.Module):
     def __init__(self, num_classes=10):
         super(bayesian, self).__init__()
-        self.classifier = nn.Sequential(
-            BayesianLinear(784, 10),
-        )
+        self.linear1 = BayesianLinear(784, 10),
+
     def forward(self, x):
         x = torch.flatten(x, 1) #1
-        x = self.classifier(x) #2
+        x = self.linear1(x) #2
         return x
+
+    def log_variational_posterior(self, input):
+        return self.linear1.weight.log_prior(input) + self.linear1.bias.log_prior(input)
 
 if __name__ == "__main__":
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -54,20 +60,7 @@ if __name__ == "__main__":
     model = bayesian().to(device)
     output = model(input.to(device))
 
-    negative_log_likehood = f.nll_loss(output, target.to(device)).to(device)
+    negative_log_likehood = f.nll_loss(f.log_softmax(output, dim=1), target.to(device)).to(device)
+    log_variational_posterior = model.log_variational_posterior()
     log_prior = 0
-    log_variational_posterior = 0
     loss = log_variational_posterior - log_prior + negative_log_likehood
-# output.mean(0)
-#
-# output
-# tmp.mean(0)
-# tmp
-# (5.2919 + 12.7009)/2
-# # loss 구하기
-#
-# weight_mu = nn.Parameter(torch.Tensor(10, 784).uniform_(-0.2, 0.2))
-# f.linear(torch.flatten(input, 1), weight_mu)
-#
-#
-# torch.log(torch.tensor([-10.0459]))
